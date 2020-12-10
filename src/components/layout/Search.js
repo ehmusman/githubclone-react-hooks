d55559267511058666faa37c41c05f814ba49b14
ehmusman <ehmusman@gmail.com>
2020-12-10 21:25:36 +0500

    Searcj.js component is functionalized with react hooks

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -1,60 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-    state = {
-        text: ''
-    }
-    static propTypes = {
-        searchUser: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired
-    }
+const Search = ({ clearUsers, showClear, setAlert, searchUser }) => {
+    const [text, setText] = useState('')
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.text === '') {
-            this.props.setAlert('danger', "Please fill all the fields")
+        if (text === '') {
+            setAlert('danger', "Please fill all the fields")
         } else {
-            this.props.searchUser(this.state.text)
-            this.setState({ text: '' })
+            searchUser(text)
+            setText('')
         }
     }
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
+    const onChange = (e) => {
+        setText(e.target.value);
     }
-    render() {
-        const { text } = this.state;
-        const { clearUsers, showClear } = this.props
-        return (
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="row">
-                        <div className="col-md-8 from-group">
-                            <input type="text" className='form-control form-control-lg'
-                                placeholder='Enter Username...'
-                                name='text'
-                                value={text}
-                                onChange={this.onChange}
-                            />
-                        </div>
-                        <div className="col-md-4 form-group">
-                            <input type="submit" value="Submit" className='btn btn-dark btn-block btn-lg' />
-                        </div>
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="row">
+                    <div className="col-md-8 from-group">
+                        <input type="text" className='form-control form-control-lg'
+                            placeholder='Enter Username...'
+                            name='text'
+                            value={text}
+                            onChange={onChange}
+                        />
                     </div>
-                </form>
-                {showClear &&
-                    <button
-                        className="btn btn-block btn-lg btn-danger"
-                        onClick={clearUsers}>
-                        Clear
+                    <div className="col-md-4 form-group">
+                        <input type="submit" value="Submit" className='btn btn-dark btn-block btn-lg' />
+                    </div>
+                </div>
+            </form>
+            {showClear &&
+                <button
+                    className="btn btn-block btn-lg btn-danger"
+                    onClick={clearUsers}>
+                    Clear
                     </button>}
 
-            </div>
-        )
-    }
+        </div>
+    )
 }
-
+Search.propTypes = {
+    searchUser: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+}
 
 export default Search
